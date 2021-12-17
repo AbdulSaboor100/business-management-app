5b65d0bb24ef583e409f24d5df787eaf878ecc7e
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2021-12-17 23:18:23 +0500

    side nav done and working on login page

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -4,6 +4,12 @@ import styles from './login.module.scss';
 const LoginComponent = () => {
     return (
         <div className={styles.main_login}>
+            <div className={styles.login_1}>
+                <input type="text" value={} onChange={(e)=>{}} />
+            </div>
+            <div className={styles.login_2}></div>
+            <div className={styles.login_3}></div>
+            <div className={styles.login_4}></div>
         </div>
     )
 }
