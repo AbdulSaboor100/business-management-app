7984dc4a3ac40edf9cacbb3d8e7b81a77f55eda2
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2021-12-18 01:05:19 +0500

    login page complete UI

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import styles from './login.module.scss';
 
 const LoginComponent = () => {
@@ -6,16 +7,23 @@ const LoginComponent = () => {
     let [password , setPassword] = useState('');
     return (
         <div className={styles.main_login}>
+        <div className={styles.main2}>
+            <div className={styles.login_5}>
+                <p>Login</p>
+            </div>
             <div className={styles.login_1}>
-                <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
+                <input placeholder='Email' type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
             </div>
             <div className={styles.login_2}>
-                <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+                <input placeholder='Password' type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
             </div>
             <div className={styles.login_3}>
                 <button>Log in</button>
             </div>
-            <div className={styles.login_4}></div>
+            <div className={styles.login_4}>
+                <p>Don't have an account <Link>create here</Link></p>
+            </div>
+         </div>
         </div>
     )
 }
--- src/screens/login/login.jsx
+++ src/screens/login/login.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Col, Row } from 'reactstrap';
 import LoginComponent from '../../components/login/login';
 import SideNavbar from '../../components/side-navbar/side-navbar';
 import styles from './login.module.scss';
 
 const MainLogin = () => {
+    
+
     return (
         <>  
             <div className={styles.container}>
                 <Row className='gx-0'>
-                    <Col sm={12} md={1} className='gx-0'>
+                    <Col xs={1} sm={1} md={1} className='gx-0' id={styles.remove}>
                         <SideNavbar />
                     </Col>         
-                    <Col sm={12 } md={11}>
+                    <Col xs={11} sm={11} md={11}>
                         <LoginComponent />
                     </Col>
                 </Row>
