8d79acb7338dffa6f1ea4008e3a9b21449c618b8
AbdulSaboor100 <70238888+AbdulSaboor100@users.noreply.github.com>
2021-12-17 23:21:04 +0500

    some changes

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './login.module.scss';
 
 const LoginComponent = () => {
+    let [email , setEmail] = useState('');
+    let [password , setPassword] = useState('');
     return (
         <div className={styles.main_login}>
             <div className={styles.login_1}>
-                <input type="text" value={} onChange={(e)=>{}} />
+                <input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}} />
             </div>
-            <div className={styles.login_2}></div>
-            <div className={styles.login_3}></div>
+            <div className={styles.login_2}>
+                <input type="text" value={password} onChange={(e)=>{setPassword(e.target.value)}} />
+            </div>
+            <div className={styles.login_3}>
+                <button>Log in</button>
+            </div>
             <div className={styles.login_4}></div>
         </div>
     )
